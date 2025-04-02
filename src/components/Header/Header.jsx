0a91001c639a058cf274725439049e69189cef59
Buse Filizler <b.filizler@digitastic.app>
2025-04-02 13:19:43 +0300

    refactor Header component for improved logo size and text styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,11 +13,13 @@ export default function Header() {
   return (
     <div className="flex flex-row items-center justify-between bg-white h-20 px-10 text-black border-b-2 border-gray-300">
       <div>
-        <img src={payMedLogo} alt="logo" className="w-30" />
+        <img src={payMedLogo} alt="logo" className="w-16 lg:w-30" />
       </div>
       <div className="flex flex-row items-center space-x-1">
-        <GppGoodOutlinedIcon fontSize="large" sx={{ color: "green" }} />
-        <span className=" text-xl">Güvenli Ödeme</span>
+        <GppGoodOutlinedIcon fontSize="medium" sx={{ color: "green" }} />
+        <span 
+        className="text-sm sm:text-base md:text-lg lg:text-xl font-semibold">
+        Güvenli Ödeme</span>
       </div>
       <div>
         <Box sx={{ top: 20, right: 20, zIndex: 1000 }}>
