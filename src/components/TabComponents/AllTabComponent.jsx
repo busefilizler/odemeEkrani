6478c42f1e0377fe5a31cf5647104890d9bc06d7
Buse Filizler <b.filizler@digitastic.app>
2025-04-02 13:16:16 +0300

    refactor padding in AllTabComponent and BodyLayout for improved layout consistency

--- src/components/TabComponents/AllTabComponent.jsx
+++ src/components/TabComponents/AllTabComponent.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import OrderDetails from "./OrderDetails";
 export default function AllTabComponent() {
   return (
-    <div className="py-8 lg:py-20 px-4 lg:pl-20 lg:pr-10 bg-gray-100 flex flex-col gap-8">
+    <div className="py-5 lg:py-20 px-2 lg:pl-20 lg:pr-10 bg-gray-100 flex flex-col gap-8">
       <CustomerInfo />
       <OrderDetails />
     </div>
--- src/layouts/BodyLayout.jsx
+++ src/layouts/BodyLayout.jsx
@@ -3,7 +3,7 @@ import AllTabComponent from '../components/TabComponents/AllTabComponent'
 import TotalCountBox from '../components/TotalCount/TotalCountBox'
 export default function BodyLayout() {
   return (
-    <div className='flex flex-col lg:flex-row px-4 lg:px-10 relative'>
+    <div className='flex flex-col lg:flex-row px-2 lg:px-10 relative'>
       <div className="bg-gray-100 flex-1">
         <AllTabComponent />
       </div>
