45b248e08a0c2af26d54c6d7b7aa722c314558d0
busefilizler <busefilizler@hotmail.com>
2025-04-03 14:40:47 +0300

    Refactor TotalCountBox component to improve layout and readability; integrate agreement checkbox and enhance mobile view

--- src/components/TotalCount/TotalCountBox.jsx
+++ src/components/TotalCount/TotalCountBox.jsx
@@ -12,104 +12,128 @@ import PrePaymentModal from "../Modals/PrePaymentModal";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
+const orderData = {
+  totalPrice: "10.000 TL",
+  itemCount: 1,
+  shippingFee: "Ücretsiz",
+  agreementText:
+    "Ön bilgilendirme formunu ve Mesafeli satış sözleşmesini okudum ve onaylıyorum.",
+};
+
 export default function TotalCountBox() {
   const [isChecked, setIsChecked] = useState(false);
   const [open, setOpen] = useState(false);
   const [isExpanded, setIsExpanded] = useState(false);
 
-  const handleCheckboxChange = (event) => {
-    setIsChecked(event.target.checked);
-  };
-
+  const handleCheckboxChange = (event) => setIsChecked(event.target.checked);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   const toggleExpand = () => setIsExpanded(!isExpanded);
 
-  // Mobil görünüm için özet içerik
-  const MobileSummary = () => (
-    <div className="flex items-center flex-col justify-between px-4 py-1 w-full">
-      <div className="pb-2 flex items-start w-full">
-        <FormControlLabel
-          control={
-            <Checkbox
-              sx={{
-                color: "black",
-                "&.Mui-checked": {
-                  color: "black",
-                },
-                opacity: isChecked ? 1 : 0.5,
-                transition: "opacity 0.3s",
-              }}
-              checked={isChecked}
-              className="!-mt-2"
-              onChange={handleCheckboxChange}
-            />
-          }
-          label={
-            <Typography sx={{ fontSize: 12 }}>
-              <span
-                style={{
-                  cursor: "pointer",
-                  textDecoration: "underline",
-                  color: "black",
-                }}
-                onClick={handleOpen}
-              >
-                Ön bilgilendirme formunu
-              </span>{" "}
-              ve Mesafeli satış sözleşmesini okudum ve onaylıyorum.
-            </Typography>
-          }
+  // Onay kutusu bileşeni
+  const AgreementCheckbox = () => (
+    <FormControlLabel
+      control={
+        <Checkbox
           sx={{
-            display: "flex",
-            paddingTop: 2,
-            flexDirection: "row",
-            alignItems: "flex-start",
+            color: "black",
+            "&.Mui-checked": { color: "black" },
             opacity: isChecked ? 1 : 0.5,
             transition: "opacity 0.3s",
           }}
+          checked={isChecked}
+          className="!-mt-2"
+          onChange={handleCheckboxChange}
         />
-      </div>
-      <div className="flex flex-row justify-between w-full pt-2 pb-1 border-t border-gray-200">
-        <div className="flex flex-col">
-          <Typography className="!font-medium" sx={{ fontSize: 12 }}>
-            Toplam Tutar (1 Ürün)
-          </Typography>
-          <Typography
-            className="!font-semibold flex justify-between"
-            sx={{ fontSize: 16 }}
+      }
+      label={
+        <Typography sx={{ fontSize: 12 }}>
+          <span
+            style={{
+              cursor: "pointer",
+              textDecoration: "underline",
+              color: "black",
+            }}
+            onClick={handleOpen}
           >
-            10.000 TL{" "}
-            {!isExpanded ? (
-              <KeyboardArrowDownIcon
-                onClick={toggleExpand}
-                className="cursor-pointer"
-              />
-            ) : (
-              <KeyboardArrowUpIcon
-                onClick={toggleExpand}
-                className="cursor-pointer"
-              />
-            )}
-          </Typography>
+            Ön bilgilendirme formunu
+          </span>{" "}
+          ve Mesafeli satış sözleşmesini okudum ve onaylıyorum.
+        </Typography>
+      }
+      sx={{
+        display: "flex",
+        paddingTop: 2,
+        flexDirection: "row",
+        alignItems: "flex-start",
+        opacity: isChecked ? 1 : 0.5,
+        transition: "opacity 0.3s",
+      }}
+    />
+  );
+
+  // Mobil görünüm
+  const MobileView = () => (
+    <Card className="w-full" variant="outlined">
+      <div className="flex items-center flex-col justify-between px-4 py-1 w-full">
+        <div className="pb-2 flex items-start w-full">
+          <AgreementCheckbox />
         </div>
-        <div>
-          <Stack spacing={2} direction="row" className="!w-full">
+        <div className="flex flex-row justify-between w-full pt-2 pb-1 border-t border-gray-200">
+          <div className="flex flex-col">
+            <Typography className="!font-medium" sx={{ fontSize: 12 }}>
+              Toplam Tutar ({orderData.itemCount} Ürün)
+            </Typography>
+            <Typography
+              className="!font-semibold flex justify-between"
+              sx={{ fontSize: 16 }}
+            >
+              {orderData.totalPrice}
+              {!isExpanded ? (
+                <KeyboardArrowDownIcon
+                  onClick={toggleExpand}
+                  className="cursor-pointer"
+                />
+              ) : (
+                <KeyboardArrowUpIcon
+                  onClick={toggleExpand}
+                  className="cursor-pointer"
+                />
+              )}
+            </Typography>
+          </div>
+          <Stack spacing={2} direction="row">
             <Button
               variant="contained"
               disabled={!isChecked}
               className="!w-full"
-              sx={{ width: "100%", backgroundColor: "black" }}
+              sx={{ backgroundColor: "black" }}
             >
               Ödeme Yap
             </Button>
           </Stack>
         </div>
       </div>
-    </div>
+      {isExpanded && (
+        <div className="border-t border-gray-200">
+          <div className="px-4 py-1">
+            <Typography className="!font-normal" sx={{ fontSize: 14 }}>
+              <div className="flex flex-row items-center justify-between">
+                <div>Ara Toplam ({orderData.itemCount} Ürün)</div>
+                <div>{orderData.totalPrice}</div>
+              </div>
+              <div className="flex flex-row items-center justify-between pt-1 text-gray-400">
+                <div>Kargo*</div>
+                <div>{orderData.shippingFee}</div>
+              </div>
+            </Typography>
+          </div>
+        </div>
+      )}
+    </Card>
   );
 
-  // Masaüstü görünümü
+  // Masaüstü görünüm
   const DesktopView = () => (
     <Box className="w-80" sx={{ minWidth: 320, minHeight: 503 }}>
       <Card className="p-2.5" variant="" sx={{ boxShadow: 1 }}>
@@ -119,49 +143,10 @@ export default function TotalCountBox() {
               Toplam Tutar
             </Typography>
             <Typography className="!font-semibold" sx={{ fontSize: 16 }}>
-              10.000 TL
+              {orderData.totalPrice}
             </Typography>
           </div>
-          <FormControlLabel
-            control={
-              <Checkbox
-                sx={{
-                  color: "black",
-                  "&.Mui-checked": {
-                    color: "black",
-                  },
-                  opacity: isChecked ? 1 : 0.5,
-                  transition: "opacity 0.3s",
-                }}
-                checked={isChecked}
-                className="!-mt-2"
-                onChange={handleCheckboxChange}
-              />
-            }
-            label={
-              <Typography sx={{ fontSize: 12 }}>
-                <span
-                  style={{
-                    cursor: "pointer",
-                    textDecoration: "underline",
-                    color: "black",
-                  }}
-                  onClick={handleOpen}
-                >
-                  Ön bilgilendirme formunu
-                </span>{" "}
-                ve Mesafeli satış sözleşmesini okudum ve onaylıyorum.
-              </Typography>
-            }
-            sx={{
-              display: "flex",
-              paddingTop: 2,
-              flexDirection: "row",
-              alignItems: "flex-start",
-              opacity: isChecked ? 1 : 0.5,
-              transition: "opacity 0.3s",
-            }}
-          />
+          <AgreementCheckbox />
         </CardContent>
         <CardActions>
           <Stack spacing={2} direction="row" className="!w-full">
@@ -188,42 +173,17 @@ export default function TotalCountBox() {
           </Typography>
           <Typography className="!font-normal" sx={{ fontSize: 12 }}>
             <div className="flex flex-row items-center justify-between pt-3">
-              <div>Sipariş Tutarı (1 Ürün)</div>
-              <div>10.000 TL</div>
+              <div>Sipariş Tutarı ({orderData.itemCount} Ürün)</div>
+              <div>{orderData.totalPrice}</div>
             </div>
             <div className="flex flex-row items-center justify-between pt-3 text-gray-400">
               <div>Kargo*</div>
-              <div>Ücretsiz</div>
+              <div>{orderData.shippingFee}</div>
             </div>
           </Typography>
         </CardContent>
       </Card>
     </Box>
-  );
-
-  // Mobil görünüm
-  const MobileView = () => (
-    <Card className="w-full" variant="outlined">
-      <div>
-        <MobileSummary />
-      </div>
-      {isExpanded && (
-        <div className="border-t border-gray-200">
-          <div className="px-4 py-1">
-            <Typography className="!font-normal" sx={{ fontSize: 14 }}>
-              <div className="flex flex-row items-center justify-between">
-                <div>Ara Toplam (1 Ürün)</div>
-                <div>10.000 TL</div>
-              </div>
-              <div className="flex flex-row items-center justify-between pt-1 text-gray-400">
-                <div>Kargo*</div>
-                <div>Ücretsiz</div>
-              </div>
-            </Typography>
-          </div>
-        </div>
-      )}
-    </Card>
   );
 
   return (
