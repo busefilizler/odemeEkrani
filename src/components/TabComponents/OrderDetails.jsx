956dc3ed6473b2edf776007725c8d1dd39356a91
busefilizler <busefilizler@hotmail.com>
2025-04-03 13:48:51 +0300

    Adjust image dimensions in OrderDetails component for improved layout consistency

--- src/components/TabComponents/OrderDetails.jsx
+++ src/components/TabComponents/OrderDetails.jsx
@@ -5,12 +5,12 @@ export default function OrderDetails() {
   return (
     <div>
       <AccordionComponent title="Sipariş Detayları">
-        <div className="flex flex-row  items-center gap-5 pb-2 w-full px-1 lg:px-3">
+        <div className="flex flex-row items-center gap-5 pb-2 w-full px-1 lg:px-3">
           <div>
             <img
               src="https://via.placeholder.com/150"
               alt="Ürün Resmi"
-              className=" w-20 lg:w-32 h-20 lg:h-32 object-cover border rounded-lg"
+              className=" w-18 lg:w-28 h-18 lg:h-28 object-cover border rounded-lg"
             />
           </div>
           <div className="flex flex-col gap-1 ">
